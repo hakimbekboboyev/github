01960e613465f34c0c0c5512ca9c7fe645090af3
hakimbekboboyev <87408338+hakimbekboboyev@users.noreply.github.com>
2021-12-07 15:47:16 +0500

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,35 @@
+n = input("N=")
+l = len(n)
+k = 0
+t = int(n)
+q = ""
+m=0
+try:
+    for i in range(l):
+        if n[i]=="1":
+            k = i
+            while n[k]=="1":
+                k = k+1
+                q=n[k::]
+                
+            break
+
+        if n[i]=="0":
+            continue
+
+    
+
+    for i in range(len(q)):
+            
+        if q[i]=="0":
+            continue
+        if  q[i]=="1":
+            m = m+1
+
+    if m==0:
+        print("YES")
+    else:
+        print("NO")
+except IndexError:
+    print("YES")
 
